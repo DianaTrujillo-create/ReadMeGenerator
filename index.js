7d8db5da5304d94884a2ed91b6513598014509e4
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-23 17:27:13 -0500

    commit to main

--- index.js
+++ index.js
@@ -5,7 +5,38 @@ const util = require('util');
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
-const questions = [
+const questions = [{
+    type: 'input',
+    message: "What is your GitHub username?",
+    name: 'username', 
+    
+    // locates github username
+}, {
+    type:"input",
+    message: "What is the title of the project?", 
+    name: "Title",
+    // create title of project
+}, { 
+    type: 'input',
+    message: "Give a detailed description of your project.",
+    name: "Description",
+    // give description of your project
+}, {
+    type: "input",
+    message: "What necessary dependencies must be installed to run this app?",
+    name: "Installation"
+    // what is required in order for app to function
+}, {
+    type: "input", 
+    message: "What is app used for?", 
+    name: "Usage",
+    // what is the app intended for
+} 
+
+]
+
+    ])
+}
 
 ];
 

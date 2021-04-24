c06c3b1fc6b0c3e1c51b3742d4f2d94ea5871ead
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-24 14:46:58 -0500

    Update generateMarkdown.js
    updating generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,51 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+  ## Table of Contents
+
+  *[Description](#description)
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[Contributing](#contributing)
+  *[Test](#test)
+  *[License](#license)
+  *[Questions](#questions)
+
+  ## Description :
+
+  ${data.Description}
+
+  ## Installation :
+  
+  To install necessary dependencies, run the following command:
+
+  \'\'\'
+  ${data.Installation}
+  \'\'\'
+
+  ## Usage 
+
+  ${data.Usage}
+
+  ${renderLicenseSection(data.License)}
+
+  ## Contributing
+
+${data.Contributing}
+
+## Test
+
+To run tests, run the following command: 
+
+\'\'\'
+${data.Test}
+\'\'\'
+
+## Questions
+
+If you have any questions about the repo, please contact me directly at ${data.email}. 
+
 `;
 }
 
